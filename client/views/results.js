1ee2a601a58e65cc4e27f3ac3ac47dbd14c8c63b
Clement Pigeon <clementpigeon@gmail.com>
2014-04-14 16:41:20 +0200

    create results views (including details using a session variable)

---
+++ client/views/results.js
@@ -0,0 +1,30 @@
+Template.results_view.results = function(){
+  return Results.find();
+};
+
+Template.results_view.details = function(){
+  return Session.get('details');
+};
+
+Template.result_details.data = function(){
+  return Session.get('details');
+};
+
+Template.result_item.events({
+    'click #details_btn': function(e) {
+        // e is the event, e.currentTarget is the button
+        // this is the template data
+
+        console.log(this);
+        Session.set('details', this);
+
+    }
+});
+
+Template.result_details.events({
+    'click #close_details_btn': function(e) {
+        Session.set('details', null);
+    }
+});
+
+
