adaf0bb9478541d6e156b425e766638cc56031ab
Clement Pigeon <clementpigeon@gmail.com>
2014-04-23 00:01:41 +0200

    add opening hours today to results_item

--- client/views/results_item.js
+++ client/views/results_item.js
@@ -16,6 +16,37 @@ Template.result_item.photo = function(){
   }
 }
 
+Template.result_item.opening_hours_today = function(){
+  if (!this.opening_hours || !this.opening_hours.periods){
+    return "Horaires d'ouverture inconnus";
+  }
+  if ((this.opening_hours.periods[0].open.day === 0) && (!this.opening_hours.periods[0].close)){
+    return "Ouvert 24h/24h";
+  }
+  var today = new Date();
+
+  var periods = this.opening_hours.periods;
+  var periods_today = _(periods).select(function(period){
+    return period.open.day === today.getDay();
+  });
+  var periods_today_strings = _(periods_today).map(function(period){
+    return "de " + format_hour(period.open.time) + " à " + format_hour(period.close.time);
+  })
+  if (periods_today[0]){
+    return "Ouvert aujourd'hui " + periods_today_strings.join(' et ');
+  } else {
+    return "Fermé aujourd'hui";
+  }
+}
+
+function format_hour(hour_string){
+  if (hour_string === '0000') return "minuit"
+  if (hour_string === '1200') return "midi"
+  var hours = hour_string[0] === '0' ? hour_string[1] : hour_string.slice(0, 2);
+  var minutes = hour_string.slice(2, 4) === '00' ? '' : hour_string.slice(2, 4);
+  return hours + 'h' + minutes;
+}
+
 Template.result_item.events({
     'click #details_btn': function(e) {
         // "this" is the template data
